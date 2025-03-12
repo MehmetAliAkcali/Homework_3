b1c654ec6daa55e49d0ce79635eb0bb3a91e7850
MehmetAliAkcali <mehmetali.akcali@std.yeditepe.edu.tr>
2025-03-13 01:36:19 +0300

    Add files via upload

---
+++ Homework_3.2/Homework_3.2/Program.cs
@@ -0,0 +1,34 @@
+﻿static void AracMuayene(int aracYasi)
+{
+    if (aracYasi >= 0 && aracYasi < 10)
+    {
+        Console.WriteLine("Arabanız yeni.");
+    }
+    else if (aracYasi >= 10 && aracYasi < 20)
+    {
+        Console.WriteLine("Servise götürmeniz gerekebilir.");
+    }
+    else if (aracYasi >= 20 && aracYasi <= 30)
+    {
+        Console.WriteLine("Arabanız hurdaya çıkabilir.");
+    }
+    else
+    {
+        Console.WriteLine("Ya hiç üretilmedi ya da trafikten men edilmiştir.");
+
+    }
+}
+
+// Aralıklar kontrol edildi.
+AracMuayene(-1);  // Ya hiç üretilmedi ya da trafikten men edilmiştir.
+AracMuayene(0);   // Arabanız yeni.
+AracMuayene(1);   // Arabanız yeni.
+AracMuayene(9);   // Arabanız yeni.
+AracMuayene(10);  // Servise götürmeniz gerekebilir.
+AracMuayene(11);  // Servise götürmeniz gerekebilir.
+AracMuayene(19);  // Servise götürmeniz gerekebilir.
+AracMuayene(20);  // Arabanız hurdaya çıkabilir.
+AracMuayene(21);  // Arabanız hurdaya çıkabilir.
+AracMuayene(29);  // Arabanız hurdaya çıkabilir.
+AracMuayene(30);  // Arabanız hurdaya çıkabilir.
+AracMuayene(31);  // Ya hiç üretilmedi ya da trafikten men edilmiştir.
